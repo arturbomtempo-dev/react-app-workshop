3ba11c7648b72d856b8a76b0f551bc70aab01a68
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-07-31 11:47:19 -0300

    fix: correct typo in footer color definition in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
                 surface: '#121212',
                 divider: '#d9d9d9',
                 'footer-bg': '#292929',
-                'footer-color': '##f7f7f7',
+                'footer-color': '#f7f7f7',
             },
         },
     },
